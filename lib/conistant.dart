c550ed6d98ce95235add3c1dafe1c1b64e443cbe
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-01-06 16:42:00 +0200

    created application and initial structure with bottomNavigationBar

---
+++ lib/conistant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/animation.dart';
+
+const kPrimaryColors = Color(0xfff7f7fa);
+const kPrimaryColorsTow = Color(0xffffffff);
---
+++ lib/features/home/presentation/Widgets/hoem_screen_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HoemScreenBody extends StatelessWidget {
+  const HoemScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      body: Column(),
+    );
+  }
+}
