5355ca4fead9aed04bd0f7d5fc43c6f73c98a730
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-01-31 18:00:41 +0200

    called BackIconWidget

--- lib/features/compare/presentation/view/widgets/compare_body.dart
+++ lib/features/compare/presentation/view/widgets/compare_body.dart
@@ -1,3 +1,4 @@
+import 'package:coinexa_app/core/utils/widgets/back_icon_widget.dart';
 import 'package:coinexa_app/features/compare/presentation/view/widgets/background_compare.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,7 @@ class CompareBody extends StatelessWidget {
       body: Stack(
         children: [
           BackgroundCompare(),
-
+          Positioned(top: 77, left: 20, child: BackIconWidget()),
         ],
       ),
     );
